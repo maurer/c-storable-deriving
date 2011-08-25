2afafe98f40a7d492fa08e3a6243e061d14a50e3
Matthew Maurer <maurer@ugcs.caltech.edu>
2011-08-25 18:49:46 -0400

    All Storable things are CStorable and take precedence over automatic derivation.

--- Foreign/CStorable.hs
+++ Foreign/CStorable.hs
@@ -3,9 +3,16 @@ module Foreign.CStorable
 ) where
 
 import Foreign.Ptr
+import Foreign.Storable
 
 class CStorable a where
   cPeek      :: Ptr a -> IO a
   cPoke      :: Ptr a -> a -> IO ()
   cAlignment :: a -> Int
   cSizeOf    :: a -> Int
+
+instance (Storable a) => CStorable a where
+  cPeek      = peek
+  cPoke      = poke
+  cAlignment = alignment
+  cSizeOf    = sizeOf
